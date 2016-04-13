921b3cdb795222aa5ddabe1f5676a6aa182c7b45
Cesar Castro <csrcastro@gmail.com>
2016-04-13 17:49:50 +0100

    add bootstrap css

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
+
 //Components
 import Ui from './components/ui/ui.js';
 import Container from './components/containers/test';
