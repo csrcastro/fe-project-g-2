fe965f0c417b3965145d52ecb60d7fa95fc2d2d3
Cesar Castro <csrcastro@gmail.com>
2016-04-14 22:44:48 +0100

    add nav component

---
+++ src/components/layout/nav.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Axios from 'axios';
+import Config from '../../config/config';
+
+import NavItem from '../ui/navItem';
+
+
+export default React.createClass({
+	getInitialState(){
+		return{
+			loading: true,
+			pages: []
+		}
+	},
+	componentWillMount(){
+		Axios.get(Config.apiUrl + 'pages').then((pages)=>{
+			this.setState({
+				loading: false,
+				pages: pages.data
+			});
+		})
+	},
+	render(){
+
+		const pages = this.state.loading ? <li>loading</li> : this.state.pages.map((page)=>{
+								return <NavItem key={page.id} path={page.slug} name={page.title.rendered}/>
+							})
+
+		return (
+			<nav className="navbar navbar-default">
+				<ul className="nav navbar-nav">
+					<NavItem path="list" name="List" />
+					{pages}
+				</ul>
+			</nav>
+		)
+	}
+})
