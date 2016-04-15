840566e44e6e6dcd084546799cdf1dfcb2ef3331
Cesar Castro <csrcastro@gmail.com>
2016-04-15 08:44:32 +0100

    update webpack config to hande our own css via sass + postcss

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
-var Webpack = require('webpack');
-var path = require('path');
-var Html = require('html-webpack-plugin');
-var htmlConfig = new Html({
+const Webpack = require('webpack');
+const path = require('path');
+const Html = require('html-webpack-plugin');
+const htmlConfig = new Html({
     template: path.resolve('src/index.html'),
     filename: "index.html",
     inject: 'body'
 });
 
-var HotModuleReplacementPlugin = new Webpack.HotModuleReplacementPlugin();
+const HotModuleReplacementPlugin = new Webpack.HotModuleReplacementPlugin();
+
+
+const autoprefixer = require('autoprefixer');
+const precss = require('precss');
 
 
 module.exports = { // Actual configuration object returned by this file, this configuration object isloade into Webpack
@@ -15,16 +19,19 @@ module.exports = { // Actual configuration object returned by this file, this co
     entry: path.resolve('src/app.js'), // Declaration of our main application file (entry point), Webpack will parse this file and look for the modules to be loaded into the bundle
 
     module: { // Here we list the options that will affect our modules before bundling
-         // A collection of automatically applied loaders (think of these as the agents that affect our modules)
+        // A collection of automatically applied loaders (think of these as the agents that affect our modules)
         loaders: [{
             test: /\.css$/,
             loaders: ['style', 'css'] // loader for our css files, we run style-loader, then the css-loader 
 
         }, {
+            test: /\.(sass|scss)$/,
+            loaders: ['style', 'css', 'postcss', 'sass']
+        }, {
             test: /\.(woff|woff2|eot|ttf|svg)$/,
             loader: 'url?limit=100000' // A loader for our fonts, this uses the url-loader (and file-loader under the hood)
 
-        }, { 
+        }, {
             test: /\.js$/,
             exclude: /node_modules/,
             loader: "babel-loader", // Our Babel loader responsible for our JSX and ES6 compilation
@@ -32,6 +39,10 @@ module.exports = { // Actual configuration object returned by this file, this co
                 presets: ["es2015", "react"] // we need to setup the presets
             }
         }]
+    },
+
+    postcss: function() {
+        return [autoprefixer, precss];
     },
 
     output: { // Tells Webpack how to write the compiled files to disk. In our case it will write to ./public/scripts.js
