1b408d8efa8348864d068c01d90874f032b24f34
Cesar Castro <csrcastro@gmail.com>
2016-04-16 10:05:35 +0100

    refactor navItem - state passed to router needed to be more pecific

--- src/components/ui/navItem.js
+++ src/components/ui/navItem.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router'
 
-const Item = (props) => <Link to={{pathname: props.slug, state:props.page}} className="btn btn-default navbar-btn text-uppercase" activeClassName="active">{props.title}</Link>
+const Item = (props) => <Link to={{pathname: props.slug, state:{page:props.page}}} className="btn btn-default navbar-btn text-uppercase" activeClassName="active">{props.title}</Link>
 
 Item.propTypes = {
 	page: React.PropTypes.object,
